a6e3577fee33df20d071479549305bb548cbac9c
Armando Di Santo <armando.di.santo@hotmail.com>
2024-08-09 12:06:36 +0200

    Selfwork PHP 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// Definizione delle variabili
+$intero = 42; // Integer
+$decimale = 3.14; // Float
+$stringa = "Ciao, mondo!"; // String
+$booleano = true; // Boolean
+
+var_dump($intero);
+var_dump($decimale);
+var_dump($stringa);
+var_dump($booleano);
+
+
+
+// Costanti
+const INTERO = 42;
+const DECIMALE = 3.14;
+const STRINGA = "Ciao, mondo!";
+const BOOLEANO = true;
+
+
+var_dump(INTERO);
+var_dump(DECIMALE);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
